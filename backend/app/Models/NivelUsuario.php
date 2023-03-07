bf2372c86b190c4f1a19aba11a52dbfb58260cdb
yuri <yurialec@hotmail.com>
2023-03-07 16:31:00 -0300

    Alterações

---
+++ backend/app/Models/NivelUsuario.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class NivelUsuario extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'descricao',
+    ];
+}
