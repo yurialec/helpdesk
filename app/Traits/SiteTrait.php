b692da2f2493505f2f3dd2b25fce20ddc325f27a
Yuri Alec <yuri.alec@hotmail.com>
2024-11-13 17:48:39 -0300

    removendo arquivos desnecessários

--- app/Traits/SiteTrait.php
+++ app/Traits/SiteTrait.php
@@ -2,52 +2,18 @@
 
 namespace App\Traits;
 
-use App\Models\Blog\Blog;
-use App\Models\Site\MainText;
 use App\Models\Site\SiteAbout;
-use App\Models\Site\SiteCarousel;
 use App\Models\Site\SiteContact;
-use App\Models\Site\SiteSocialMedia;
 
 trait SiteTrait
 {
-    public function mainText()
-    {
-        return MainText::first();
-    }
-
-    public function carousels()
-    {
-        return SiteCarousel::get();
-    }
-
     public function contactSite()
     {
         return SiteContact::first();
     }
 
-    public function socialmedias()
-    {
-        return SiteSocialMedia::get();
-    }
-
     public function aboutSite()
     {
         return SiteAbout::first();
-    }
-
-    public function blogs()
-    {
-        return Blog::orderBy('id', 'DESC')->get();
-    }
-
-    public function blogById($id)
-    {
-        return Blog::find($id);
-    }
-
-    public function siteblogs()
-    {
-        return Blog::orderBy('id', 'DESC')->take(3)->get();
     }
 }
