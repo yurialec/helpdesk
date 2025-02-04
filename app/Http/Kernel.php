142cf687c46fa8ea1261c5dcb3c83357b510f711
Yuri Alec <yuri.alec@hotmail.com>
2025-02-04 08:24:43 -0300

    removida implementações de tenant

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,7 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'identify.tenant' => \App\Http\Middleware\IdentifyTenant::class,
         'acl' => \App\Http\Middleware\Acl::class,
     ];
 }
